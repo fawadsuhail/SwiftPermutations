18e08b2b0240879a24f66a26eabc00e8e1833185
Fawad Suhail <fawadsuhail@gmail.com>
2016-06-10 00:09:55 +0400

    Added test case

--- SwiftPermutationsTests/SwiftPermutationsTests.swift
+++ SwiftPermutationsTests/SwiftPermutationsTests.swift
@@ -7,30 +7,43 @@
 //
 
 import XCTest
+import Foundation
+
 @testable import SwiftPermutations
 
 class SwiftPermutationsTests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testPermutation() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let array = [1, 2, 4, 5]
+    
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
+    let permutations = appDelegate.permutations(array)
+    
+    XCTAssert(permutations.count == factorial(array.count))
+  }
+  
+  func factorial(n: Int) -> Int {
+    
+    if n == 0 {
+      return 1
+    } else {
+      return n * factorial(n - 1)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
 }
