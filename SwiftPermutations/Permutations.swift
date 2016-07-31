e6ae22af7eadeb474c4cee1a345e07fb83b4de73
Fawad Suhail <fawadsuhail@gmail.com>
2016-07-31 10:27:24 +0400

    Fixed spacing

--- SwiftPermutations/Permutations.swift
+++ SwiftPermutations/Permutations.swift
@@ -11,32 +11,25 @@ import Foundation
 extension Array {
   
   func permutations() -> [[Element]] {
-    
     if count == 1 {
       return [[self[0]]]
     }
-    
     // lock the first element
     let firstElement = self[0]
-    
     // remove the locked element from the array
     var subArray = self
     subArray.removeFirst()
-    
     var subPermutations = subArray.permutations()
     var results = [[Element]]()
     
     for i in 0..<subPermutations.count {
       for j in 0...subPermutations[i].count {
-        
         var subResultArray = subPermutations[i]
-        
         if j < subResultArray.count {
           subResultArray.insert(firstElement, atIndex: j)
         } else {
           subResultArray.append(firstElement)
         }
-        
         results.append(subResultArray)
       }
     }
